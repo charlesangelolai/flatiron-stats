ca0ae6ff1a24f7cd75bf6a9a3e526576dd6780ee
Charles Lai <charlesangeloplai@gmail.com>
2021-01-23 23:50:41 -0500

    adds routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :survey_responses
+  resources :survey_questions
+  resources :surveys
+  resources :cohorts
+  resources :projects
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
