8ea4a7ed886b10ac6dc8f32a15ec2f7b00dcc7dd
Charles Lai <charlesangeloplai@gmail.com>
2021-01-26 04:13:45 -0500

    adds projects views and controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,61 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    params[:project][:user_id] = current_user.id
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      flash.now[:error] = @project.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to project_path(@project)
+    else
+      flash.now[:error] = @project.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def find_project
+    @project = Project.find_by_id(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(
+      :name,
+      :phase_num,
+      :description,
+      :website_link,
+      :github_link,
+      :blog_link,
+      :video_link,
+      :user_id
+    )
+  end
 end
