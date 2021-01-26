30a2c4b97396ea71112c7010eb436f22aac60e6f
Charles Lai <charlesangeloplai@gmail.com>
2021-01-26 04:10:08 -0500

    updates path names and adds root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  :path_names => {
+    :sign_in => 'login',
+    :sign_out => 'logout',
+    :sign_up => 'signup' }
+
   resources :survey_responses
   resources :survey_questions
   resources :surveys
   resources :cohorts
   resources :projects
+
+  root to: "static#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
