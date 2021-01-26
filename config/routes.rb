e52bf75828673438171be019fad9ed5d0fe39284
Charles Lai <charlesangeloplai@gmail.com>
2021-01-25 23:46:30 -0500

    replaces users setup with devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :survey_responses
   resources :survey_questions
   resources :surveys
   resources :cohorts
   resources :projects
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
