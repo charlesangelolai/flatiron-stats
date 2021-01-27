3e16feca75032e5dce1bc518fcdf5b8a9ca9ad32
Charles Lai <charlesangeloplai@gmail.com>
2021-01-27 02:15:45 -0500

    adds model associations and nested attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  belongs_to :cohort
+  belongs_to :role
+  has_many :projects
+  has_many :survey_responses, through: :projects
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  accepts_nested_attributes_for :cohort, reject_if: :all_blank
 end
