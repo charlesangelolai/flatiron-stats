12eabe511ec6bbca612bc6eab60c03e24d6546d2
Charles Lai <charlesangeloplai@gmail.com>
2021-01-27 02:01:08 -0500

    adds application helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :cohort_id, cohort_attributes: [:name, :program, :time]])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def is_admin?
+    current_user.role_id == 1
+  end
+
+  def is_instructor?
+    current_user.role_id == 2
+  end
 end
