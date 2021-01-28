aa17e15bb864800b8539548c6db9abf7b1734af1
Charles Lai <charlesangeloplai@gmail.com>
2021-01-28 10:36:56 -0500

    removes association and acceptance of nested attribute

--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -1,5 +1,5 @@
 class SurveyQuestion < ApplicationRecord
   belongs_to :survey
 
-  accepts_nested_attributes_for :survey, reject_if: :all_blank
+  # accepts_nested_attributes_for :survey, reject_if: :all_blank
 end
--- app/models/survey_response.rb
+++ app/models/survey_response.rb
@@ -1,5 +1,4 @@
 class SurveyResponse < ApplicationRecord
   belongs_to :survey
   belongs_to :project
-  belongs_to :user
 end
