608ca2d66e6c578e7b4163d1fdbedb647b485468
Charles Lai <charlesangeloplai@gmail.com>
2021-01-28 05:24:19 -0500

    updates surveys model, views, and migration files

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,4 @@
 class Survey < ApplicationRecord
-  belongs_to :project
   has_many :survey_questions
   has_many :survey_responses
 end
--- db/migrate/20210124042359_create_surveys.rb
+++ db/migrate/20210124042359_create_surveys.rb
@@ -1,6 +1,7 @@
 class CreateSurveys < ActiveRecord::Migration[6.1]
   def change
     create_table :surveys do |t|
+      t.string :name
 
       t.timestamps
     end
