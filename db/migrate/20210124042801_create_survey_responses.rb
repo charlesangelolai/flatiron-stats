db6a8ce10db9b4abf9de662c16782ccb0fcbfc89
Charles Lai <charlesangeloplai@gmail.com>
2021-01-29 19:05:54 -0500

    updates survey responses migration file

--- db/migrate/20210124042801_create_survey_responses.rb
+++ db/migrate/20210124042801_create_survey_responses.rb
@@ -1,7 +1,6 @@
 class CreateSurveyResponses < ActiveRecord::Migration[6.1]
   def change
     create_table :survey_responses do |t|
-      t.integer :phase_num
       t.integer :design_response
       t.integer :navigation_response
       t.integer :error_handling_response
