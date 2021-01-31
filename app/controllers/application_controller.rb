91f8abc68fbdbd29c6f69d1b80dc818fce24f95d
Charles Lai <charlesangeloplai@gmail.com>
2021-01-31 05:32:13 -0500

    updates controllers to work with public-activity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  include ApplicationHelper
+  include PublicActivity::StoreController
+  include ApplicationHelper 
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
