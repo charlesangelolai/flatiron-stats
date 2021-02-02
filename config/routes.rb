6442a8babd621e716b789f84c0b5153cab2fa2ad
Charles Lai <charlesangeloplai@gmail.com>
2021-02-02 02:56:46 -0500

    adds nested routes for watercooler section

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  resources :comments
+  resources :replies
+  resources :posts
+  resources :categories
   devise_for :users,
   :path_names => {
     :sign_in => 'login',
@@ -17,13 +21,21 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :cohorts
+  resources :surveys
   resources :survey_responses
   resources :survey_questions
-  resources :surveys
-  resources :cohorts
-  resources :projects
+  
+  resources :projects, except: [:show]
   resources :projects, only: [:show] do
     resource :survey_responses
+  end
+
+  resources :categories, except: [:show]
+  resources :categories, only: [:show] do
+    resources :posts do
+      resources :comments
+    end
   end
 
   get '/dashboard', to: 'dashboard#index'
