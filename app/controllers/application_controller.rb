5a4c1f44bc754eee5b6dbcae79b03434b3a4828e
Charles Lai <charlesangeloplai@gmail.com>
2021-02-03 02:35:43 -0500

    updates devise permited parameters and user edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :cohort_id, cohort_attributes: [:name, :program, :time]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :cohort_id, cohort_attributes: [:name, :program, :time]], except: [:password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
