74965678f3a3bf59108d6f3b1542106d96fae64b
Charles Lai <charlesangeloplai@gmail.com>
2021-02-03 02:41:26 -0500

    adds omniauth database migration file and updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_004301) do
+ActiveRecord::Schema.define(version: 2021_02_02_204749) do
 
   create_table "activities", force: :cascade do |t|
     t.string "trackable_type"
