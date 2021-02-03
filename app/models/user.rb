4cd82b5f0d3e09b83f5dcfe151b1f690738ee47f
Charles Lai <charlesangeloplai@gmail.com>
2021-02-03 02:44:50 -0500

    updates belongs to cohort associations to optional: true

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   include PublicActivity::Model
   tracked except: :update, owner: ->(controller, model) { controller && controller.current_user }
 
-  belongs_to :cohort
+  belongs_to :cohort, optional: true
   belongs_to :role
   has_many :projects
   has_many :survey_responses, through: :projects
@@ -10,10 +10,19 @@ class User < ApplicationRecord
   has_many :comments, through: :posts
   
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, and :omniauthable
+  # :confirmable, :lockable, :timeoutable 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :trackable
+         :trackable, :omniauthable, omniauth_providers: [:github]
   
   accepts_nested_attributes_for :cohort, reject_if: :all_blank
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.first_name = auth.info.name.split(' ').first
+      user.last_name = auth.info.name.split(' ').second
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end      
+  end
 end
