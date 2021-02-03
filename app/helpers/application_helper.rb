30c8d79abdb82db8f6d811e69e8cfb55b941b0a5
Charles Lai <charlesangeloplai@gmail.com>
2021-02-03 02:43:56 -0500

    adds cohort validation helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def is_instructor?
     current_user.role_id == 2
   end
+
+  def redirect_if_no_cohort
+    unless current_user.cohort_id
+      redirect_to edit_user_registration_path
+    end
+  end
 end
