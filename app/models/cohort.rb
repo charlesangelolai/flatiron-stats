8e5079159e46f9f76abf1802de3fa84b839d0a51
Charles Lai <charlesangeloplai@gmail.com>
2021-02-04 15:52:33 -0500

    fixes cohort creation and  validation errors

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -5,6 +5,4 @@ class Cohort < ApplicationRecord
   has_many :users
 
   validates :name, presence: true, uniqueness: true
-  validates :program, presence: true
-  validates :time, presence: true
 end
