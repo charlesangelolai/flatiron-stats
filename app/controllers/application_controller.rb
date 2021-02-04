1d515cfb4db4e149bf3e5c429b63a05544c12c71
Charles Lai <charlesangeloplai@gmail.com>
2021-02-04 15:51:30 -0500

    fixes user validations and cohort creation errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,30 +31,4 @@ class ApplicationController < ActionController::Base
       redirect_to edit_user_registration_path
     end
   end
-
-  def format_date(year, month, day)
-    year = (year.to_i % 100).to_s
-    month.prepend("0") unless month.length > 1
-    day.prepend("0") unless day.length > 1
-    full_date = month + day + year
-    
-    full_date
-  end
-
-  def format_cohort_name(date, program, time)
-    @COHORT_PROGRAMS = {
-      "Software Engineering" => "seng",
-      "Data Science" => "dsci",
-      "Cyber Security" => "csec"
-    }
-    
-    @COHORT_TIME = {
-      "Full-Time" => "ft",
-      "Part-Time" => "pt"
-    }
-
-    cohort_name = "#{@COHORT_PROGRAMS[program]}-#{@COHORT_TIME[time]}-#{date}"
-    
-    cohort_name
-  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,6 +12,30 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
+    unless params[:user][:cohort_attributes].values.any?("")
+      year = params[:user][:cohort_attributes]["name(1i)"]
+      month = params[:user][:cohort_attributes]["name(2i)"]
+      day = params[:user][:cohort_attributes]["name(3i)"]
+      program = params[:user][:cohort_attributes][:program]
+      time = params[:user][:cohort_attributes][:time]
+  
+      params[:user][:cohort_attributes].delete("name(1i)")
+      params[:user][:cohort_attributes].delete("name(2i)")
+      params[:user][:cohort_attributes].delete("name(3i)")
+      
+      full_date = format_date(year, month, day)
+      params[:user][:cohort_attributes][:name] = format_cohort_name(full_date, program, time)
+    end
+    super
+  end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  def update
     year = params[:user][:cohort_attributes]["name(1i)"]
     month = params[:user][:cohort_attributes]["name(2i)"]
     day = params[:user][:cohort_attributes]["name(3i)"]
@@ -23,19 +47,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
     params[:user][:cohort_attributes].delete("name(3i)")
     
     full_date = format_date(year, month, day)
-    params[:user][:cohort_attributes][:name] = format_cohort_name(full_date, program, time)
+
+    if full_date
+      params[:user][:cohort_attributes][:name] = format_cohort_name(full_date, program, time)
+    end
     super
   end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
 
   # DELETE /resource
   # def destroy
@@ -84,4 +101,34 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  private
+
+  def format_date(year, month, day)
+    unless [year, month, day].any?("")
+      year = (year.to_i % 100).to_s
+      month.prepend("0") unless month.length > 1
+      day.prepend("0") unless day.length > 1
+      full_date = month + day + year
+      
+      full_date
+    end
+  end
+
+  def format_cohort_name(date, program, time)
+    @COHORT_PROGRAMS = {
+      "Software Engineering" => "seng",
+      "Data Science" => "dsci",
+      "Cyber Security" => "csec"
+    }
+    
+    @COHORT_TIME = {
+      "Full-Time" => "ft",
+      "Part-Time" => "pt"
+    }
+
+    cohort_name = "#{@COHORT_PROGRAMS[program]}-#{@COHORT_TIME[time]}-#{date}"
+    
+    cohort_name
+  end
 end
