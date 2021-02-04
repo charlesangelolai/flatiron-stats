f9bb17aa821cb32d8b96441e25df20dfd0144b45
Charles Lai <charlesangeloplai@gmail.com>
2021-02-03 19:26:06 -0500

    adds model validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,14 @@ class Project < ApplicationRecord
   include PublicActivity::Model
   tracked owner: ->(controller, model) { controller && controller.current_user }
 
+  validates :name, presence: true
+  validates :phase_num, presence: true
+  validates :description, presence: true
+  validates :website_link, presence: true
+  validates :github_link, presence: true
+  validates :blog_link, presence: true
+  validates :video_link, presence: true
+
   belongs_to :user
   has_many :survey_responses
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,5 +2,8 @@ class Survey < ApplicationRecord
   has_many :survey_questions
   has_many :survey_responses
 
+  validates :name, presence: true
+  validates :phase_num, presence: true
+
   accepts_nested_attributes_for :survey_questions, reject_if: :all_blank
 end
