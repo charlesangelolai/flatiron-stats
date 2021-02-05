aa7c5d3c6055dc92e6105b939948dc800c0dd63b
Charles Lai <charlesangeloplai@gmail.com>
2021-02-04 21:19:24 -0500

    adds helper method for alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
   def is_instructor?
     current_user.role_id == 2
   end
+
+  def alert_class_by_type(alert_type)
+    case alert_type.to_sym
+    when :alert, :danger, :error, :validation_errors
+      'alert-danger'
+    when :warning, :todo
+      'alert-warning'
+    when :notice, :success
+      'alert-success'
+    else
+      'alert-info'
+    end
+  end
 end
